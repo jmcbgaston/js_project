c089b60daef9cf65868943b9944e2ce59106dc92
Jose Gaston <jmcbgaston@gmail.com>
2020-11-03 20:28:46 -0800

    Add music

---
+++ grid.js
@@ -0,0 +1,12 @@
+class Grid {
+    construction() {
+        this.dim = 16 // denotes 16x16
+
+        // this.character = x
+        // this.shield = x
+    }
+
+    createGrid() {
+        
+    }
+}
--- main.js
+++ main.js
@@ -2,5 +2,15 @@ function startGame() {
     console.log("hello")
 
     // this function starts the game
-    // start renders a new instance of board which loads the other elements
+    // start renders a new instance of board which loads the other
+}
+
+function play() {
+    const audio = document.getElementById('audio')
+    audio.play();
+}
+
+function pause() {
+    const audio = document.getElementById('audio')
+    audio.pause();
 }
